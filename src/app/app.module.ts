a78d87b678f4971696d960b3da9b4e8d5a954a93
Kauã Marques Rob <kaua@outlook.com>
2018-12-02 12:49:49 -0200

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,65 +1,61 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from "../pages/about/about";
+import { ContactPage } from "../pages/contact/contact";
+import { HomePage } from "../pages/home/home";
+import { TabsPage } from "../pages/tabs/tabs";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { ExplorarPageModule } from '../pages/explorar/explorar.module';
-import { PerfilPageModule } from '../pages/perfil/perfil.module';
-import { MensagensPageModule } from '../pages/mensagens/mensagens.module';
-import { HistoricoPageModule } from '../pages/historico/historico.module';
-import { IntroPageModule } from '../pages/intro/intro.module';
-import { OpcoesInicialPageModule } from '../pages/opcoes-inicial/opcoes-inicial.module';
-import { OpcoesInicialPage } from '../pages/opcoes-inicial/opcoes-inicial';
-import { TelaLoginPage } from '../pages/tela-login/tela-login';
-import { TelaLoginPageModule } from '../pages/tela-login/tela-login.module';
-import { CriarDadosPessoaisPage } from '../pages/criar-dados-pessoais/criar-dados-pessoais';
-import { CriarDadosPessoaisPageModule } from '../pages/criar-dados-pessoais/criar-dados-pessoais.module';
-import { TodosFavPage } from '../pages/todos-fav/todos-fav';
-import { TodosFavPageModule } from '../pages/todos-fav/todos-fav.module';
-import { PerfilProPage } from '../pages/perfil-pro/perfil-pro';
-import { PerfilProPageModule } from '../pages/perfil-pro/perfil-pro.module';
-import { EsqueciSenhaPage } from '../pages/esqueci-senha/esqueci-senha';
-import { EsqueciSenhaPageModule } from '../pages/esqueci-senha/esqueci-senha.module';
-import { EdicaoPerfilPageModule } from '../pages/edicao-perfil/edicao-perfil.module';
-import { EdicaoPerfilPage } from '../pages/edicao-perfil/edicao-perfil';
-import { MensagensPage } from '../pages/mensagens/mensagens';
-import { ConversaPageModule } from '../pages/conversa/conversa.module';
-import { ConversaPage } from '../pages/conversa/conversa';
-import { TrocarSenhaPageModule } from '../pages/trocar-senha/trocar-senha.module';
-import { TrocarSenhaPage } from '../pages/trocar-senha/trocar-senha';
-import { ExplorarServicosPageModule } from '../pages/explorar-servicos/explorar-servicos.module';
-import { ExplorarServicosPage } from '../pages/explorar-servicos/explorar-servicos';
-import { ServicoPageModule } from '../pages/servico/servico.module';
-import { ServicoPage } from '../pages/servico/servico';
-import { CadastrarServicoPage } from '../pages/cadastrar-servico/cadastrar-servico';
-import { CadastrarServicoPageModule } from '../pages/cadastrar-servico/cadastrar-servico.module';
-import { AvaliarServicoPageModule } from '../pages/avaliar-servico/avaliar-servico.module';
-import { AvaliarServicoPage } from '../pages/avaliar-servico/avaliar-servico';
-import { BrMaskerModule } from 'brmasker-ionic-3';
-import { AngularFireModule } from 'angularfire2'; 
-import { AngularFireAuth } from 'angularfire2/auth';
-import { firebaseConfig } from '../config';
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { ExplorarPageModule } from "../pages/explorar/explorar.module";
+import { PerfilPageModule } from "../pages/perfil/perfil.module";
+import { MensagensPageModule } from "../pages/mensagens/mensagens.module";
+import { HistoricoPageModule } from "../pages/historico/historico.module";
+import { IntroPageModule } from "../pages/intro/intro.module";
+import { OpcoesInicialPageModule } from "../pages/opcoes-inicial/opcoes-inicial.module";
+import { OpcoesInicialPage } from "../pages/opcoes-inicial/opcoes-inicial";
+import { TelaLoginPage } from "../pages/tela-login/tela-login";
+import { TelaLoginPageModule } from "../pages/tela-login/tela-login.module";
+import { CriarDadosPessoaisPage } from "../pages/criar-dados-pessoais/criar-dados-pessoais";
+import { CriarDadosPessoaisPageModule } from "../pages/criar-dados-pessoais/criar-dados-pessoais.module";
+import { TodosFavPage } from "../pages/todos-fav/todos-fav";
+import { TodosFavPageModule } from "../pages/todos-fav/todos-fav.module";
+import { PerfilProPage } from "../pages/perfil-pro/perfil-pro";
+import { PerfilProPageModule } from "../pages/perfil-pro/perfil-pro.module";
+import { EsqueciSenhaPage } from "../pages/esqueci-senha/esqueci-senha";
+import { EsqueciSenhaPageModule } from "../pages/esqueci-senha/esqueci-senha.module";
+import { EdicaoPerfilPageModule } from "../pages/edicao-perfil/edicao-perfil.module";
+import { EdicaoPerfilPage } from "../pages/edicao-perfil/edicao-perfil";
+import { MensagensPage } from "../pages/mensagens/mensagens";
+import { ConversaPageModule } from "../pages/conversa/conversa.module";
+import { ConversaPage } from "../pages/conversa/conversa";
+import { TrocarSenhaPageModule } from "../pages/trocar-senha/trocar-senha.module";
+import { TrocarSenhaPage } from "../pages/trocar-senha/trocar-senha";
+import { ExplorarServicosPageModule } from "../pages/explorar-servicos/explorar-servicos.module";
+import { ExplorarServicosPage } from "../pages/explorar-servicos/explorar-servicos";
+import { ServicoPageModule } from "../pages/servico/servico.module";
+import { ServicoPage } from "../pages/servico/servico";
+import { CadastrarServicoPage } from "../pages/cadastrar-servico/cadastrar-servico";
+import { CadastrarServicoPageModule } from "../pages/cadastrar-servico/cadastrar-servico.module";
+import { AvaliarServicoPageModule } from "../pages/avaliar-servico/avaliar-servico.module";
+import { AvaliarServicoPage } from "../pages/avaliar-servico/avaliar-servico";
+import { BrMaskerModule } from "brmasker-ionic-3";
 
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuth } from "angularfire2/auth";
+import { firebaseConfig } from "../config";
+import { AuthService } from "../services/auth.service";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
+  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp,{tabsHideOnSubPages: true}),
+    IonicModule.forRoot(MyApp, { tabsHideOnSubPages: true }),
     AngularFireModule.initializeApp(firebaseConfig.fire),
     ExplorarPageModule,
     PerfilPageModule,
@@ -80,7 +76,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CadastrarServicoPageModule,
     AvaliarServicoPageModule,
     BrMaskerModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -107,8 +103,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AngularFireAuth
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AngularFireAuth,
+    AuthService
   ]
 })
 export class AppModule {}
--- src/pages/tela-login/tela-login.ts
+++ src/pages/tela-login/tela-login.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { TabsPage } from '../tabs/tabs';
-import { EsqueciSenhaPage } from '../esqueci-senha/esqueci-senha';
-import { ToastController } from 'ionic-angular';
-import { FormGroup } from '@angular/forms';
-import { AlertController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { TabsPage } from "../tabs/tabs";
+import { EsqueciSenhaPage } from "../esqueci-senha/esqueci-senha";
+import { ToastController } from "ionic-angular";
+import { FormGroup } from "@angular/forms";
+import { AlertController } from "ionic-angular";
+import { AuthService } from "../../services/auth.service";
 
 /**
  * Generated class for the TelaLoginPage page.
@@ -15,72 +16,67 @@ import { AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-tela-login',
-  templateUrl: 'tela-login.html'
+  selector: "page-tela-login",
+  templateUrl: "tela-login.html"
 })
-
-
 export class TelaLoginPage {
-
-  
   form: FormGroup;
-  senha:any;
-  email:any;
+  senha: any;
+  email: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public toastCtrl: ToastController, public alertCtrl: AlertController) {
-  this.email="";
-  this.senha="";
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController,
+    private auth: AuthService
+  ) {
+    this.email = "";
+    this.senha = "";
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TelaLoginPage');
-  }
-  
-  
-
- 
-
-  goToTabsPage(position: string){
-    console.log(this.auth)
-    if(this.email=="" && this.senha==""){
-      const alert = this.alertCtrl.create({
-        title: 'Algo errado!',
-        subTitle: 'Você não digitou suas informações de login',
-        buttons: ['OK']
-      });
-      alert.present();
-    }   
-    
-    
-
-    else if(this.senha==""){
-      const alert = this.alertCtrl.create({
-        title: 'Algo errado!',
-        subTitle: 'Não esqueça de inserir senha',
-        buttons: ['OK']
-      });
-      alert.present();
-    }
-
-    else{
-    
-    this.navCtrl.push(TabsPage); 
-      let toast = this.toastCtrl.create({
-        message: 'Bem-vindo(a) de volta Kantan',
-        duration: 3000,
-        position: position
-      });
-  
-      toast.present(toast);}
-    }
-
-    
-  
-
-  
-  
-  goToEsqueciSenhaPage(){
-    this.navCtrl.push(EsqueciSenhaPage); 
+    console.log("ionViewDidLoad TelaLoginPage");
   }
 
+  goToTabsPage(position: string) {
+    if (this.email == "" && this.senha == "") {
+      const alert = this.alertCtrl.create({
+        title: "Algo errado!",
+        subTitle: "Você não digitou suas informações de login",
+        buttons: ["OK"]
+      });
+      alert.present();
+    } else if (this.senha == "") {
+      const alert = this.alertCtrl.create({
+        title: "Algo errado!",
+        subTitle: "Não esqueça de inserir senha",
+        buttons: ["OK"]
+      });
+      alert.present();
+    } else {
+      let credentials = {
+        email: this.email,
+        password: this.senha
+      };
+      let self = this;
+      this.auth.signInWithEmail(credentials).then(
+        () => {
+          self.navCtrl.push(TabsPage);
+          let toast = self.toastCtrl.create({
+            message: "Bem-vindo(a) de volta Kantan",
+            duration: 3000,
+            position: position
+          });
+
+          toast.present(toast);
+        },
+        error => console.log
+      );
+    }
+  }
+
+  goToEsqueciSenhaPage() {
+    this.navCtrl.push(EsqueciSenhaPage);
+  }
 }
